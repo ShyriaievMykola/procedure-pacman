09669f05a2e321d627314edf522e7d3dd69f1930
Ihor Svyrydenko <aetheraerys@gmail.com>
2026-02-03 13:24:41 +0200

    Фікс для інших розширень екрану (потрібно тестування)

--- visualisation/visualizer.py
+++ visualisation/visualizer.py
@@ -9,14 +9,21 @@ class Visualizer:
         pygame.init()
         self.map = map_gen
         
-        info = pygame.display.Info()
-        self.cell = info.current_w // map_gen.width
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        screen_width = self.screen.get_width()
+        screen_height = self.screen.get_height()
+        
+        # Розмір комірки базується на ширині екрану, щоб карта помістилася по горизонталі
+        self.cell = screen_width // map_gen.width
+        
+        # Центруємо карту по горизонталі, якщо є зайве місце
+        total_map_width = map_gen.width * self.cell
+        self.x_offset = (screen_width - total_map_width) // 2
         
         self.y, self.vel = 0, 0
-        self.max_y = max(0, map_gen.height * self.cell - self.screen.get_height())
+        self.max_y = max(0, map_gen.height * self.cell - screen_height)
         self.speed = self.cell // CC.SPEED_DIVISOR
-
+        
     def _draw_points(self, x, y, sx, sy):
         is_p = (x in (1, self.map.width-2) and y in (1, self.map.height-2))
         div = GC.POWER_PELLET_SIZE_DIVISOR if is_p else GC.DOT_SIZE_DIVISOR
@@ -24,7 +31,7 @@ class Visualizer:
         
         r = max(min_s, self.cell // div)
         pygame.draw.circle(self.screen, C.DOT, (sx + self.cell//2, sy + self.cell//2), r)
-
+        
     def draw(self):
         self.screen.fill(C.TUNNEL)
         sh = self.screen.get_height()
@@ -34,10 +41,10 @@ class Visualizer:
             # Рендеримо тільки те, що потрапляє в зону видимості екрана
             if -self.cell < sy < sh:
                 for x in range(self.map.width):
-                    sx = x * self.cell
+                    sx = x * self.cell + self.x_offset
                     rect = pygame.Rect(sx, sy, self.cell, self.cell)
                     cell, pos = self.map.grid[y][x], (x, y)
-
+                    
                     # Вхід в гост хаус
                     if pos == self.map.ghost_door:
                         pygame.draw.rect(self.screen, C.DOOR, rect)
@@ -60,18 +67,18 @@ class Visualizer:
                         self._draw_points(x, y, sx, sy)
                         
         pygame.display.flip()
-
+        
     def _is_gh(self, x, y):
         gx, gy = self.map.ghost_x, self.map.ghost_y
         return gx[0] <= x <= gx[1] and gy[0] <= y <= gy[1]
-
+        
     def run(self):
         clock = pygame.time.Clock()
         while True:
             for e in pygame.event.get():
                 if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
                     pygame.quit(); sys.exit()
-
+                    
             keys = pygame.key.get_pressed()
             target = (keys[pygame.K_s] - keys[pygame.K_w]) * self.speed
             
